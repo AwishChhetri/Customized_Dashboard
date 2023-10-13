451e36136c8517367ee07f0ac64d409eac465e14
AwishChhetri <abishchhetri.2502@gmail.com>
2023-10-13 15:34:30 +0530

    routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import { Routes,Route  } from 'react-router-dom';
-import Home from './components/Home.jsx';
+import { Routes, Route } from 'react-router-dom';
+import  Home  from './components/Home.jsx';
 import SignUp from './components/SignUp.jsx';
-import Login from './components/Login.jsx';
-
+import Login  from './components/login.jsx';
 
 function App() {
   return (
     <div className="App">
-    
-        <Routes>
-        <Route path='/' component={Home} exact />
-        <Route path='/login' component={Login} />
-        <Route path='/signup' component={SignUp} />
-        </Routes>
-    
-       
-
+       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
-
