15c960ce3f1f1b35c50b3506d216f742f3e4a8f8
AwishChhetri <abishchhetri.2502@gmail.com>
2023-10-17 21:39:13 +0530

    Db connected

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors');
-
+const mongoose=require('mongoose')
 
 const app = express()
 const port = 5000
@@ -10,15 +10,74 @@ app.use(express.json())
 app.use(cors())
 
 
-app.post('/register',(req,res)=>{
-    const username = req.body.username
-    const email = req.body.email
-    const password = req.body.password
-    console.log(username,email,password)
-    res.json({msg:"Success"})
+
+
+mongoose.connect("mongodb+srv://abishchhetri2502:HMHpKTGa4wPLkr3Y@cluster0.3igf2ot.mongodb.net/?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
+    console.log('connected successful')}).catch((err)=>{
+        console.log(err)
 })
 
-app.get('/',(req,res)=>{
+
+const userSchema=mongoose.Schema({
+    username:{
+        type:String
+    },
+    email:{
+        required:true,
+        type:String
+    },
+    password:{
+        type:String
+    },
+    
+
+},
+
+{
+    timestamps:true
+});
+
+const User=new mongoose.model('User', userSchema);
+ 
+app.post('/register', (req, res) => {
+    console.log(req.body);
+  
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    });
+  
+    newUser.save()
+      .then(() => {
+        console.log('User saved successfully');
+        res.json({ msg: "Success" });
+      })
+      .catch((err) => {
+        console.log('Error saving user:', err);
+        res.status(500).json({ msg: "Error" });
+      });
+  });
+
+
+app.post("/login",async(req,res)=>{
+    console.log(req.body)
+    const result=await User.findOne({email:req.body.email});
+   console.log(result)
+    if(!result)
+     {
+      res.json({msg:"Not registered"})}
+    else { 
+        if(result.password==req.body.password){
+            res.json(result)
+        }
+        else{
+            res.json({msg:"Wrong Password"})
+        }
+       
+    } })
+  
+app.get('/hello',(req,res)=>{
     res.send("HELLO, Try Route '/register'")
 })
 
