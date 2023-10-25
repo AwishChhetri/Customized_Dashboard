ac6554b45b3f49ec2bdd7fd251bf591ce8ba2467
raoyashwant <raoyashwant132@gmail.com>
2023-10-25 14:21:56 +0530

    loading

--- frontend/src/components/dash.jsx
+++ frontend/src/components/dash.jsx
@@ -7,6 +7,7 @@ import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
 import axios from "axios";
 import LoadColors from '../components/colors'
+import Loading from "./loading"
 
 const HeroPage = () => {
 
@@ -15,6 +16,7 @@ const HeroPage = () => {
   const { id } = useParams()
   const [mail,setMail] = useState('')
   const [name,setName] = useState('')
+  const [loading,setLoading] = useState(true)
 
   const [button,setButton] = useState('block rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-purple-500 transition hover:bg-indigo-700 focus:outline-none focus:ring')
   const [header,setHeader] = useState('')
@@ -29,6 +31,7 @@ const HeroPage = () => {
       setMail(res.data.email)
       setName(res.data.username)
       changeUI(res.data.textColor,res.data.buttonBackgroundColor,res.data.headerBackgroundColor,res.data.footerBackgroundColor,res.data.dropDownButtonColor,res.data.radioButtonColor)
+      setLoading(false)
     })
     .catch((err)=>{
       console.log(err)
@@ -46,35 +49,39 @@ const HeroPage = () => {
 
 
   return (
-    <div  className="bg-gradient-to-t from-white via-purple-100 to-indigo-200">
-      <div className="p-4 sm:ml-64 font-one bg-gradient-to-t from-white via-purple-100 to-indigo-200">
-        <div className={`p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 ${header}`}>
-          <Header name={name} button={button}/>
-        </div>
-        <Hero/>
-        <div className="grid grid-cols-2 gap-4 mb-4 ">
-            <div className="flex items-center flex-col justify-center rounded  h-28  ">
-              <p className="lg:text-2xl">My email:{mail}</p>
-              <p className=" flex   font-two lg:flex-row lg:text-sm sm:text-xs flex-col sm:text-clip sm:overflow-auto  text-black ">
-                <Radiobutton name="Indian" radio={radio}/>
-              </p>
-            </div>
-            <div className="flex flex-col items-center justify-center rounded  h-28 ">
-              <p className="lg:text-2xl">Id:{id}</p>
-              <p className="lg:text-sm font-two text-black  flex flex-col ">
-                <DropDown
-                  name="Languages"
-                  option1="Hindi"
-                  option2="English"
-                  option3="Japanese"
-                  drop={dropdown}
-                />
-              </p>
-            </div>
+    <>
+      {loading && <Loading/>}
+      {!loading && <div  className="bg-gradient-to-t from-white via-purple-100 to-indigo-200">
+        <div className="p-4 sm:ml-64 font-one bg-gradient-to-t from-white via-purple-100 to-indigo-200">
+          <div className={`p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 ${header}`}>
+            <Header name={name} button={button}/>
           </div>
-        <Footer foot={footer}/>
-      </div>
-    </div>
+          <Hero/>
+          <div className="grid grid-cols-2 gap-4 mb-4 ">
+              <div className="flex items-center flex-col justify-center rounded  h-28  ">
+                <p className="lg:text-2xl">My email:{mail}</p>
+                <p className=" flex   font-two lg:flex-row lg:text-sm sm:text-xs flex-col sm:text-clip sm:overflow-auto  text-black ">
+                  <Radiobutton name="Indian" radio={radio}/>
+                </p>
+              </div>
+              <div className="flex flex-col items-center justify-center rounded  h-28 ">
+                <p className="lg:text-2xl">Id:{id}</p>
+                <p className="lg:text-sm font-two text-black  flex flex-col ">
+                  <DropDown
+                    name="Languages"
+                    option1="Hindi"
+                    option2="English"
+                    option3="Japanese"
+                    drop={dropdown}
+                  />
+                </p>
+              </div>
+            </div>
+          <Footer foot={footer}/>
+        </div>
+      </div>}
+    </>
+    
   );
 };
 
